5bfe958f51b9032511f343bc8e328d064ce1d963
Garrett <ga.landin89@gmail.com>
2022-05-03 16:34:39 -0500

    Registered patient model with admin site + Completed POST functionality for Patient

--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,7 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PatientSerializer
+from .models import Patient
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def patients_list(request):
-
-    return Response('ok')
+    if request.method == 'GET':
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
