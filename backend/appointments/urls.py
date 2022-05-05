72b52a84e4f05afaa8258e9ad91478f8c79a7475
Garrett <ga.landin89@gmail.com>
2022-05-05 17:12:13 -0500

    Updated appointment dates url to be shorter

--- backend/appointments/urls.py
+++ backend/appointments/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.appointments_list),
     path('<int:pk>/', views.appointment_detail),
-    path('appointmentdates/', views.appointmentdate_list),
+    path('dates/', views.appointmentdate_list),
 ]
