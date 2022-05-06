2d399c5a94e87f24c10f9f3fa3c482cd1f19d9da
Garrett <ga.landin89@gmail.com>
2022-05-06 11:23:26 -0500

    Updated application name in Nav Bar + Created search bar component file and added some initial code

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = ({handleSubmit, input , setInput}) => {
+    
+    return ( 
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type='text' value={input} onChange={(event) => setInput(event.target.value)}/>
+                <button type='submit'>Search</button>
+            </form>
+        </div>
+     );
+}
+ 
+export default SearchBar;
