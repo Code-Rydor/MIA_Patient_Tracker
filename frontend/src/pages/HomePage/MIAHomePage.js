e4be37bfddf5d4ed04aed17e244187ad62a1d9ba
Garrett <ga.landin89@gmail.com>
2022-05-06 12:10:45 -0500

    Created MIAHomePage and RescheduleFormPage files + Added initial code to both new files

---
+++ frontend/src/pages/HomePage/MIAHomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+const MIAHomePage = () => {
+    return ( 
+        <h1>Test Text</h1>
+     );
+}
+ 
+export default MIAHomePage;
---
+++ frontend/src/pages/RescheduleFormPage/RescheduleFormPage.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import useAuth from '../../hooks/useAuth';
+import useCustomForm from '../../hooks/useCustomForm';
+
+
+let initialValues = {
+    first_name: "",
+    last_name: "",
+    phone_number: "",
+    email: "",
+};
+
+const RescheduleFormPage = () => {
+    const [user, token] = useAuth()
+    const navigate = useNavigate()
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, postNewAppointment)
+
+    async function postNewAppointment() {
+        try {
+            let response = await axios.post("http://127.0.0.1:8000/api/patients/", formData, {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            // navigate('/')
+        } catch (error) {
+            console.log(error.message)
+            
+        }
+    }
+
+
+    return ( 
+        <div className="container">
+            <h2>{user.username}</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <label>
+                    First Name:{" "}
+                    <input
+                        type="text"
+                        name="first_name"
+                        value={formData.first_name}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Last Name:{" "}
+                    <input
+                        type="text"
+                        name="last_name"
+                        value={formData.last_name}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Phone Number:{" "}
+                    <input
+                        type="text"
+                        name="phone_number"
+                        value={formData.phone_number}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Email:{" "}
+                    <input
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <button>Add Patient</button>
+            </form>
+        </div>
+     );
+}
+ 
+export default RescheduleFormPage;
