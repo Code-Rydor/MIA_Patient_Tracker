6746c29d90d43907b37cc8eb8cbfa793d86f8970
Garrett <ga.landin89@gmail.com>
2022-05-09 23:25:08 -0500

    Installed email API package + Created Email API component file

--- frontend/src/pages/ToBeContactedPage/ToBeContactedPage.js
+++ frontend/src/pages/ToBeContactedPage/ToBeContactedPage.js
@@ -5,8 +5,6 @@ import DisplaySearchedUsers from '../../components/DisplaySearchedUsers/DisplayS
 import SearchForUser from '../../components/SearchForUser/SearchForUser';
 import axios from "axios";
 
-//This page will show the table containing list of patients to be contacted
-//! Consider using table from Video Games
 
 const ToBeContactedPage = () => {
 
@@ -14,6 +12,7 @@ const ToBeContactedPage = () => {
     const [users, setUsers] = useState([]);
     const [input, setInput] = useState('');
     const [filteredUsers, setFilteredUsers] = useState([])
+    const [patchedUser, setPatchedUser] = useState([])
     
     useEffect(() => {
         getAllUsers();
@@ -31,20 +30,38 @@ const ToBeContactedPage = () => {
         console.log(error.message);
     }
     };
+
     const handleSubmit = (event) => {
       event.preventDefault()
       let filteredResults = users.filter((user) => {
         if (user.first_name.toLowerCase() == input.toLowerCase() ||
         user.last_name.toLowerCase() == input.toLowerCase() ||
         user.phone_number.includes(input))
-          return true
+              return true
+          console.log(filteredResults)
       })
       setFilteredUsers(filteredResults)
     };
+
+    const patchToBeContacted = async (pk = filteredUsers.id) => {
+        try {
+            let response = await axios.patch(`http://127.0.0.1:8000/api/auth/users/patch/${pk}/`, filteredUsers.is_tobecontacted = true, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            console.log(response.data)
+            setPatchedUser(response.data)
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return ( 
         <div>
             <SearchForUser handleSubmit={handleSubmit} input={input} setInput={setInput} />
-            <DisplaySearchedUsers filteredUsers={filteredUsers} input={input} />
+            <DisplaySearchedUsers patchedUser={patchedUser} input={input}
+               patchToBeContacted={patchToBeContacted} token={token} />
         </div>
      );
 }
