35122656d4f4a7e393eb3b1e5be99790a077ea03
Garrett <ga.landin89@gmail.com>
2022-05-11 17:29:42 -0500

    Uninstalled sendgrid/mail package + Installed emailjs/browser package + Updated emailpage file with new email form + Successfully sent and received email from emailpage.js file

--- frontend/src/pages/EmailPage/EmailPage.js
+++ frontend/src/pages/EmailPage/EmailPage.js
@@ -1,75 +1,33 @@
-import React from 'react';
-import { Row, Col, Form, Input, Button, Space } from 'antd';
-import 'antd/dist/antd.css';
-import '../../sendgrid.env';
-
-const { TextArea } = Input;
-
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const EmailPage = () => {
-    const [form] = Form.useForm();
-    return ( 
-        <Row gutter={24} style={{ padding: '30px' }}>
-            <Col xl={12}>
-                <Form layout='vertical' form={form}>
-                    <Form.Item
-                        name='name'
-                        label='Name'
-                        rules={[
-                            {
-                                required: true
-                            }
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name='email'
-                        label='Email'
-                        rules={[
-                            {
-                                required: true
-                            }
-                        ]}
-                    >
-                        <Input />
-                        <Form.Item
-                        name='subject'
-                        label='Subject'
-                        rules={[
-                            {
-                                required: true
-                            }
-                        ]}
-                    >
-                            <Input />
-                            <Form.Item
-                        name='message'
-                        label='Message'
-                        rules={[
-                            {
-                                required: true
-                            }
-                        ]}
-                    >
-                        <TextArea />
-                    </Form.Item>
-                    </Form.Item>
-                    </Form.Item>
-                    <Form.Item>
-                        <Space>
-                            <Button type='primary'>Submit</Button>
-                            <Button type='secondary' onClick={(e) => form.resetFields}>
-                                Clear
-                            </Button>
-                        </Space>
-                    </Form.Item>
-                </Form>
-            </Col>
-        </Row>
-     );
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, form.current, process.env.REACT_APP_YOUR_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+          console.log('Message Sent')
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+    return (
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    );
 }
  
 export default EmailPage;
