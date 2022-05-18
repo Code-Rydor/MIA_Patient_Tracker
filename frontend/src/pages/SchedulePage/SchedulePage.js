b6b07ab35975abbcc95510959fc80d814e804b06
Garrett <ga.landin89@gmail.com>
2022-05-18 16:49:47 -0500

    Added some demo event to calendar + Working on map function to pull database info into calendar

--- frontend/src/pages/SchedulePage/SchedulePage.js
+++ frontend/src/pages/SchedulePage/SchedulePage.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 import FullCalendar from '@fullcalendar/react';
 import daygridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
 
-export default () => {
+
+
+
+const ToBeContactedPage = () => {
+
     const handleDateClick = (dateClickInfo) => {
         console.log(dateClickInfo.dateStr);
     }
-    return (
-        <FullCalendar
+
+    const [user, token] = useAuth();
+    const [appointments, setAppointments] = useState([])
+    const [index, setIndex] = useState(0)
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        getAppointments();
+    }, [token]);
+
+    async function getAppointments() {
+        try {
+            let response = await axios.get("http://127.0.0.1:8000/api/appointments/", {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            setAppointments(response.data)
+
+            console.log("Appointments:", response.data)
+            console.log("Patient: ", response.data[0].patient_id.first_name)
+            console.log("Date: ", response.data[0].appointment_day_id.date)
+            console.log("Time: ", response.data[0].appointment_time)
+        } catch (error) {
+            console.log(error.message)
+        }
+    };
+
+    return ( 
+        <div>
+            <FullCalendar
             plugins={[daygridPlugin, interactionPlugin]}
-            dateClick={handleDateClick}
-        />
-    )
+                dateClick={handleDateClick}
+            //     {dates.map((option) => <option value={option.date}>{option.date}</option>)}
+            // {appointments.map((el) => events={[{ title: {el.patient_id.first_name}, date: {el.appointment_day_id.date}})}
+            events={[
+                { title: 'Event 1', date: '2022-05-01' },
+                { title: 'event 2', date: '2022-05-02' }
+              ]}
+            />
+        </div>
+     );
 }
+ 
+export default ToBeContactedPage;
