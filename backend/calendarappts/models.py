abc587651997c60db18ebbd2b425cffb6f3344de
Garrett <ga.landin89@gmail.com>
2022-05-23 15:25:37 -0500

    Created calendarappts model + Registered calendarappts in project_settings installed apps section

--- backend/calendarappts/models.py
+++ backend/calendarappts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class CalendarAppt(models.Model):
+    name = models.CharField(max_length=50)
+    appt_date = models.CharField(max_length=25)
+    appt_time = models.CharField(max_length=15)
+    
